0e999f01349b46d53399b19c08118330a6157662
deadhecore <dm.podporin@gmail.com>
2022-10-04 22:20:08 +0300

    cat and mouse game, cat initialised, mouse added on the map

--- game_in_console/game_in_console/game_in_console.c
+++ game_in_console/game_in_console/game_in_console.c
@@ -2,44 +2,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <stdlib.h>
 
 int main()
 
 {
 
-	char mas[20][40];
+	char map[20][40];
 	int i;
-	int x = 10, y = 5;
-	char key;
+	int cat_x = 10, cat_y = 5;
+	int mouse_x = 7, mouse_y = 7;
 
+	char key;
 
 	do
 
 	{
 
-		sprintf(mas[0], "---------------------------------------");
+		sprintf(map[0], "---------------------------------------");
 
 		for (i = 1; i < 19; i++)
-			sprintf(mas[i], "|                                     |");
+			sprintf(map[i], "|                                     |");
 
-		sprintf(mas[19], "---------------------------------------");
+		sprintf(map[19], "---------------------------------------");
 
-		mas[y][x] = 'X';
+		map[cat_y][cat_x] = 'X';
+		map[mouse_y][mouse_x] = '@';
 
 		key = _getch();
 
 		system("cls");
 		for (i = 0; i < 20; i++)
-			printf("%s\n", mas[i]);
+			printf("%s\n", map[i]);
 
-		if (key == 'w') y--;
-		if (key == 's') y++;
-		if (key == 'a') x--;
-		if (key == 'd') x++;
+		int cat_track_x = cat_x;
+		int cat_track_y = cat_y;
+
+		if (key == 'w') cat_y-- & mouse_y--;
+		if (key == 's') cat_y++;
+		if (key == 'a') cat_x--;
+		if (key == 'd') cat_x++;
+
+		if (map[cat_y][cat_x] == '|' || map[cat_y][cat_x] == '-')
+		{ 
+			cat_y = cat_track_y;
+			cat_x = cat_track_x;
+		}
+
 	}
 
 	while (key != 'e');
-
 
 	return 0;
 

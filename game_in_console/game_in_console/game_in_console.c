93d2a8089ef9584004f8364c42aebb4c5dd97e60
deadhecore <dm.podporin@gmail.com>
2022-10-07 12:38:08 +0300

    BUGs resolved GOC-11 Cat location unexpected changed GOC-12 Catch didn't happend GOC-13 done but can be smaller then initial and when area changed to smsller empry area from the initial aray filled by some garbage

--- game_in_console/game_in_console/game_in_console.c
+++ game_in_console/game_in_console/game_in_console.c
@@ -9,36 +9,43 @@
 int main()
 
 {
+	srand(time(0));
 
-	char map[20][40];
+	enum { map_rows = 10, map_columns = 10 };
+
+	char map[map_rows][map_columns];
 	int i;
-	int cat_x = 10, cat_y = 5;
-	int mouse_x = 7, mouse_y = 7;
+
+	// define map borders (game area = all area incide borders)
+
+	int topMB = 0;
+	int bottomMB = map_rows - 1;
+	int leftMB = 0;
+	int rightMB = map_columns - 1;
+
+	//inotialise cat and mouse
+
+	int cat_x = (rand() % (rightMB - 2) + 1), cat_y = rand() % (bottomMB - 2) + 1;
+	int mouse_x = (rand() % (rightMB - 2) + 1), mouse_y = rand() % (bottomMB - 2) + 1;
+
 	int mouseMovement;
 	char key;
 	int score = 0;
 
-	srand(time(0));
 
 	do
 
 	{
+		// empty map creation
 
-		sprintf(map[0], "---------------------------------------");
+		sprintf(map[topMB], "---------");
 
-		for (i = 1; i < 19; i++)
-			sprintf(map[i], "|                                     |");
+		for (i = topMB+1; i < bottomMB; i++)
+			sprintf(map[i], "|       |");
 
-		sprintf(map[19], "---------------------------------------");
-
-		map[cat_y][cat_x] = 'X';
-		map[mouse_y][mouse_x] = '@';
+		sprintf(map[bottomMB], "---------");
 
 		key = _getch();
-
-		system("cls");
-		for (i = 0; i < 20; i++)
-			printf("%s\n", map[i]);
 
 		//cat movement
 
@@ -50,36 +57,51 @@ int main()
 		if (key == 'a') cat_x--;
 		if (key == 'd') cat_x++;
 
+		// border tracking for cat
+
 		if (map[cat_y][cat_x] == '|' || map[cat_y][cat_x] == '-')
 		{
 			cat_y = cat_track_y;
 			cat_x = cat_track_x;
 		};
 
+		//mouse catch tracking
 
 		if (cat_y == mouse_y && cat_x == mouse_x)
 		{
-			mouse_x = rand() % 37 + 1;
-			mouse_y = rand() % 17 + 1;
+			mouse_x = rand() % (rightMB-2) + 1;
+			mouse_y = rand() % (bottomMB-2) + 1;
 			score++;
 		}
 
-		printf("Mouse caught %i \n", score);
-
 		//mouse movement
+
+		int mouse_track_x = mouse_x;
+		int mouse_track_y = mouse_y;
 
 		do
 
 		{
-			mouseMovement = (rand() % 20);
+			mouseMovement = (rand() % 4);
 
-			if (mouseMovement < 6 && mouse_y > 1) mouse_y--;
-			if (mouseMovement < 11 && 5 < mouseMovement && mouse_y < 18) mouse_y++;
-			if (mouseMovement < 16 && 10 < mouseMovement && mouse_x > 1) mouse_x--;
-			if (15 < mouseMovement && mouse_x < 38) mouse_x++;
-		} while (mouse_y < 1 || mouse_y > 19 || mouse_x < 1 || mouse_x > 39);
+			if (mouseMovement == 0 && mouse_y > 1 && !((mouse_y -1 == cat_y) && mouse_x == cat_x)) mouse_y--;
+			if (mouseMovement == 1 && (mouse_y < (bottomMB-1)) && !((mouse_y + 1 == cat_y) && mouse_x == cat_x)) mouse_y++;
+			if (mouseMovement == 2 && mouse_x > 1 && !((mouse_x - 1 == cat_x) && mouse_y == cat_y)) mouse_x--;
+			if (mouseMovement == 3 && (mouse_x < (rightMB -2)) && !((mouse_x + 1 == cat_x) && mouse_y == cat_y)) mouse_x++;
 
-		printf("mouse movement and location %i,% i,%i \n", mouseMovement, mouse_y, mouse_x);
+		} while (mouse_y < 1 || mouse_y > bottomMB || mouse_x < 1 || mouse_x > rightMB || (mouse_x == mouse_track_x && mouse_y == mouse_track_y) || (cat_y == mouse_y && cat_x == mouse_x));
+
+		map[cat_y][cat_x] = 'Q';
+		map[mouse_y][mouse_x] = '*';
+
+		//printing
+
+		system("cls");
+		for (i = 0; i < 20; i++)
+			printf("%s\n", map[i]);
+		printf("Mouse caught %i \n", score);
+		printf("Mouse movement and location %i,% i,%i \n", mouseMovement, mouse_y, mouse_x);
+		printf("Cat movement and location % i,%i \n", cat_y, cat_x);
 
 	}
 
